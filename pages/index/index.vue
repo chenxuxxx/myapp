44aa85d6e82a4a915b6cc045b21f751e3517b831
chenshu <chenxuxxx@sina.com>
2021-01-29 14:50:49 +0800

    初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="content">
+		<!-- <image class="logo" src="/static/logo.png"></image> -->
+		<view class="text-area">
+			<input v-model="phone" placeholder="请输入电话号码" class="inputText" type="number"/>
+			<button type="primary" @click="callPhone">拨打</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				phone: ''
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			callPhone() {
+				uni.makePhoneCall({
+				    phoneNumber: this.phone+'' //仅为示例
+				});
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		.text-area {
+			position: absolute;
+			top: 50%;
+			transform: translateY(-50%);
+			.inputText {
+				height: 80rpx;
+				line-height: 80rpx;
+				background-color: #eee;
+			}
+		}
+	}
+
+</style>
